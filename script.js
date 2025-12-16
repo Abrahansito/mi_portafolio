9ddd9b600b79a2192936bb75e40d970eaecf2a1b
Abrahansito <huamancarlosabraham@gmail.com>
2025-12-16 01:10:24 -0500

    Arreglando la ruta fetch

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ Swal.fire({
 });
 
   try {
-    const response = await fetch("https://portfolio-backend-0fut.onrender.com/api/contact", {
+    const response = await fetch("https://portfolio-backend-0fut.onrender.com/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
